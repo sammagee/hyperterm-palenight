b11e4e0af59bb656ee151d8544a36edc225cde15
Sam Magee <s@sammagee.me>
2016-11-04 15:12:38 -0500

    Version 1.0.1

---
+++ index.js
@@ -0,0 +1,42 @@
+
+const backgroundColor = '#292D3E'
+const foregroundColor = '#959DCB'
+const cursorColor = '#FFCC00'
+const borderColor = backgroundColor
+
+const colors = {
+	black        : backgroundColor,
+	red          : '#F07178',
+	green  		 : '#C3E88D',
+	yellow       : '#FFCB6B',
+	blue         : '#82AAFF',
+	magenta      : '#C792EA',
+	cyan         : '#89DDFF',
+	white        : foregroundColor,
+	lightBlack   : '#434758',
+	lightRed     : '#FF8B92',
+	lightGreen   : '#DDFFA7',
+	lightYellow  : '#FFE585',
+	lightBlue    : '#9CC4FF',
+	lightMagenta : '#E1ACFF',
+	lightCyan    : '#A3F7FF',
+	colorCubes   : '#ffffff',
+	grayscale    : foregroundColor
+}
+
+exports.decorateConfig = config => {
+	return Object.assign({}, config, {
+		foregroundColor,
+		backgroundColor,
+		borderColor,
+		cursorColor,
+		colors,
+		termCSS: `
+			${config.termCSS || ''}
+			.cursor-node {
+				mix-blend-mode: difference;
+				border-left-width: 2px;
+			}
+		`,
+	}
+}
